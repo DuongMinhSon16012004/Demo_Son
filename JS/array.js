bcbd40688831ce922236f80710285b87ca0e474e
DuongMinhSon16012004 <sondmhe186291@fpt.edu.vn>
2024-02-08 10:49:18 +0700

    add file js into a new branch

---
+++ JS/array.js
@@ -0,0 +1,13 @@
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+    'Java'
+]
+
+console.log(languages.toString);
+console.log(languages.join(' '));
+console.log(languages.pop);
+console.log(languages.push('C++'));
+console.log(languages.shift);
+console.log(languages.unshift('Python'));
