b19a3b275ebddd6fa7ea0744254325f867b0e7b2
DuongMinhSon16012004 <sondmhe186291@fpt.edu.vn>
2024-02-08 11:08:45 +0700

    add file string.js

---
+++ JS/string.js
@@ -0,0 +1,8 @@
+var myString = 'Hello world, my name is Son';
+console.log(myString.length);
+console.log(myString.indexOf('world'));
+console.log(myString.replace('Hello', 'Hi'));
+console.log(myString.toLowerCase());
+console.log(myString.toUpperCase());
+console.log(myString.trim());
+console.log(myString.split(' '));
